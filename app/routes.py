0e344eac893f57fbb032f52989e4f06d6e85f685
Dmytro Kondratyk <dmutrokondratyk2008@gmail.com>
2025-10-18 22:40:12 +0300

    finar version catalog

--- app/routes.py
+++ app/routes.py
@@ -23,26 +23,7 @@ def contacts():
 
 @main.route('/catalog')
 def catalog():
-    initial_cards = [
-        {"name": "Гра1", "description": "Опис гри 1", "price": 35000, "image": "https://geekach.com.ua/content/uploads/images/nastolnye-strategii.jpg"},
-        {"name": "Гра2", "description": "Опис гри 2", "price": 15000, "image": "https://geekach.com.ua/content/uploads/images/nastolnye-strategii.jpg"},
-        {"name": "Гра3", "description": "Опис гри 3", "price": 25000, "image": "https://geekach.com.ua/content/uploads/images/nastolnye-strategii.jpg"},
-        {"name": "Гра4", "description": "Опис гри 4", "price": 45000, "image": "https://geekach.com.ua/content/uploads/images/nastolnye-strategii.jpg"},
-        {"name": "Гра5", "description": "Опис гри 5", "price": 55000, "image": "https://geekach.com.ua/content/uploads/images/nastolnye-strategii.jpg"},
-        {"name": "Гра6", "description": "Опис гри 6", "price": 65000, "image": "https://geekach.com.ua/content/uploads/images/nastolnye-strategii.jpg"},
-    ]
 
-    for card in initial_cards:
-        exists = Desktop.query.filter_by(name=card["name"]).first()
-        if not exists:
-            local_image = download_image(card["image"])
-            new_desktop = Desktop(
-                name=card["name"],
-                description=card["description"],
-                price=card["price"],
-                image=local_image
-            )
-            db.session.add(new_desktop)
 
     db.session.commit()
     desktops = Desktop.query.all()
