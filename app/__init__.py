f4b315578984a7e60e7fc28f2f727c16ff885594
Pear Land <136347333+PearLand-1@users.noreply.github.com>
2025-12-09 17:51:57 +0200

    add swagger

--- app/__init__.py
+++ app/__init__.py
@@ -13,19 +13,32 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/images')
     app.config['SECRET_KEY'] = 'your_secret_key_here'
+    app.config['SWAGGER'] = {
+        'title': 'My API',
+        'uiversion': 3,
+        # 'openapi': '3.0.2'  # якщо хочеш OpenAPI 3
+    }
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from app.models.desktop import Desktop
-    from app.models.cart import CartItem
-    from app.models.order import Order
-    from app.models.news import News
-    from app.models.feedback import Feedback
-    from app.models.user import User
+    from .models.desktop import Desktop
+    from .models.cart import CartItem
+    from .models.order import Order
+    from .models.news import News
+    from .models.feedback import Feedback
+    from .models.user import User
 
-    from app.routes import main
+    from .routes import main
     app.register_blueprint(main)
+    
+    from .routes_api import api as api_bp
+    # Реєструємо blueprint з API
+    app.register_blueprint(api_bp)
+    
+    from flasgger import Swagger
+    Swagger(app)
+
 
     with app.app_context():
         db.create_all()
--- app/routes_api.py
+++ app/routes_api.py
@@ -1,3 +1,27 @@
-from flask import render_template, jsonify, g, request
+from flask import Blueprint, render_template, jsonify, g, request, session
+from app.models.user import User
 from app.service.news_service import NewsService
 from app.service.cart_service import CartService
+
+api = Blueprint('api', __name__, url_prefix='/api/v1')
+
+@api.before_app_request
+def load_current_user():
+    user_id = session.get('user_id')
+    g.current_user = User.query.get(user_id) if user_id else None
+
+@api.route("/cart", methods=["GET"])
+def get_cart():
+    """
+    Отримати корзину
+    ---
+    tags:
+      - Cart
+    responses:
+      200:
+        description: Повертає кошик користувача
+    """
+    print(g.current_user.id)
+    user_id = g.current_user.id  # Припускаємо, що ідентифікатор користувача доступний через g.current_user
+    cart_details = CartService.get_cart(user_id)
+    return jsonify(cart_details)
