0102918c9cd9f7c1de2b959663327447b351aaaa
DanyloLyk <danylolyk@gmail.com>
2025-12-16 00:51:25 +0200

    Nginx configurated and add postman files

--- app/__init__.py
+++ app/__init__.py
@@ -136,4 +136,7 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
+    print("-" * 50)
+    print("🚀 САЙТ ПРАЦЮЄ ТУТ: http://127.0.0.1")
+    print("-" * 50)
     app.run(host="0.0.0.0", port=5000, debug=True)
