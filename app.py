3e68f3bdf0399130cf485d9f88a84f8ca582eb20
DanyloLyk <danylolyk@gmail.com>
2025-12-16 01:24:05 +0200

    fix autologin in api.html and add logs for docker

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    print("-" * 50)
+    print("🚀 САЙТ ПРАЦЮЄ ТУТ: http://127.0.0.1")
+    print("-" * 50)
     app.run(host="0.0.0.0", port=5000, debug=True)
